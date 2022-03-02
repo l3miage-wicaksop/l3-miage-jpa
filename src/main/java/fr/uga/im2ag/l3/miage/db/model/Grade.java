629f178b5e2e9a738c407cb086b719eb20dd12a3
wicaksop <pradityo.wicaksono@etu.univ-grenoble-alpes.fr>
2022-03-02 18:26:06 +0100

    recent changes

--- src/main/java/fr/uga/im2ag/l3/miage/db/model/Grade.java
+++ src/main/java/fr/uga/im2ag/l3/miage/db/model/Grade.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package fr.uga.im2ag.l3.miage.db.model;
 
 import javax.persistence.Column;
@@ -71,49 +70,3 @@ public class Grade {
         return this;
     }
 }
-=======
-package fr.uga.im2ag.l3.miage.db.model;
-
-import javax.persistence.Column;
-
-// TODO ajouter une named query pour une des requêtes à faire dans le repository
-public class Grade {
-
-    private Long id;
-    private Subject subject;
-    @Column(name = "grade")
-    private Float value;
-    private Float weight;
-
-    public Long getId() {
-        return id;
-    }
-
-    public Subject getSubject() {
-        return subject;
-    }
-
-    public Grade setSubject(Subject subject) {
-        this.subject = subject;
-        return this;
-    }
-
-    public Float getValue() {
-        return value;
-    }
-
-    public Grade setValue(Float value) {
-        this.value = value;
-        return this;
-    }
-
-    public Float getWeight() {
-        return weight;
-    }
-
-    public Grade setWeight(Float weight) {
-        this.weight = weight;
-        return this;
-    }
-}
->>>>>>> 01da5a4671cb1862f37c0b6c415f564ba7c9d013
